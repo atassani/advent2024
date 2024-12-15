0f7415dbddea74cdfe8fff229f0400870a437d4d
Toni Tassani <toni.tassani@ocado.com>
2024-12-15 20:06:30 +0100

    Day8 Part One

---
+++ src/day08/Node.java
@@ -0,0 +1,31 @@
+package day08;
+
+import java.util.Objects;
+
+public class Node {
+    public final int x;
+    public final int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Node node = (Node) obj;
+        return x == node.x && y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ src/day08/ResonantCollinearity.java
@@ -0,0 +1,103 @@
+package day08;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class ResonantCollinearity {
+
+    public static final String FILE_NAME = "resources/day08/input.txt";
+
+    public static void main(String[] args) {
+        ResonantCollinearity template = new ResonantCollinearity();
+        template.run();
+    }
+
+    private static List<String> readLines() {
+        try {
+            return Files.readAllLines(Paths.get(FILE_NAME));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void run() {
+        List<String> lines = readLines();
+        int maxX = lines.get(0).length();
+        int maxY = lines.size();
+        Map<Character, List<Node>> frequencyMap = buildFrequencyMap(lines);
+        Map<Character, List<Node>> resonancesMap = getResonances(frequencyMap, maxX, maxY);
+
+        Set<Node> uniqueResonances = new HashSet<>();
+        for (List<Node> resonances : resonancesMap.values()) {
+            uniqueResonances.addAll(resonances);
+        }
+
+        List<Node> sortedResonances = new ArrayList<>(uniqueResonances);
+        sortedResonances.sort((n1, n2) -> {
+            if (n1.y != n2.y) {
+                return Integer.compare(n1.y, n2.y);
+            } else {
+                return Integer.compare(n1.x, n2.x);
+            }
+        });
+
+        for (Node resonance : sortedResonances) {
+            System.out.println(resonance);
+        }
+
+        System.out.println("Total unique resonances: " + uniqueResonances.size());
+    }
+
+    private Map<Character, List<Node>> getResonances(Map<Character, List<Node>> frequencyMap, int maxX, int maxY) {
+        Map<Character, List<Node>> resonancesMap = new HashMap<>();
+
+        for (Map.Entry<Character, List<Node>> entry : frequencyMap.entrySet()) {
+            char frequency = entry.getKey();
+            List<Node> nodes = entry.getValue();
+            List<Node> resonances = new ArrayList<>();
+
+            for (int i = 0; i < nodes.size(); i++) {
+                for (int j = i + 1; j < nodes.size(); j++) {
+                    Node node1 = nodes.get(i);
+                    Node node2 = nodes.get(j);
+
+                    int dx = node2.x - node1.x;
+                    int dy = node2.y - node1.y;
+
+                    Node resonance1 = new Node(node1.x - dx, node1.y - dy);
+                    Node resonance2 = new Node(node2.x + dx, node2.y + dy);
+
+                    if (isWithinBounds(resonance1, maxX, maxY)) {
+                        resonances.add(resonance1);
+                    }
+                    if (isWithinBounds(resonance2, maxX, maxY)) {
+                        resonances.add(resonance2);
+                    }
+                }
+            }
+
+            resonancesMap.put(frequency, resonances);
+        }
+
+        return resonancesMap;
+    }
+
+    private boolean isWithinBounds(Node node, int maxX, int maxY) {
+        return node.x >= 0 && node.x < maxX && node.y >= 0 && node.y < maxY;
+    }
+
+    private Map<Character, List<Node>> buildFrequencyMap(List<String> lines) {
+        Map<Character, List<Node>> frequencyMap = new HashMap<>();
+        for (int y = 0; y < lines.size(); y++) {
+            for (int x = 0; x < lines.get(0).length(); x++) {
+                char c = lines.get(y).charAt(x);
+                if (c != '.') {
+                    frequencyMap.computeIfAbsent(c, k -> new ArrayList<>()).add(new Node(x, y));
+                }
+            }
+        }
+        return frequencyMap;
+    }
+}
